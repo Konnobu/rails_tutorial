9bb5f19195d363a8f93a24dc7626da793613e821
Konno <dra.mixx.nico@gmail.com>
2016-05-09 21:45:16 +0900

    [readed] pp.~63 ~ch.2

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,3 +1,14 @@
 class HelloController < ApplicationController
-  render text: 'こんにちは、世界！'
+  def index
+    render text: 'こんにちは、世界！'
+  end
+
+  def view
+    @msg = '世界の車窓から'
+  end
+
+  def list
+    # TODO: あとで改善する
+    @books = Book.all
+  end
 end
